0c7190ea0cc55e03c9d818ccbe67656c4dd6e6e2
Trasqual <59961702+Trasqual@users.noreply.github.com>
2022-05-20 22:16:00 +0300

    Added Breakable Obstacle

--- Assets/Scripts/Obstacles/Obstacle.cs
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] GameObject barrier;
+    [SerializeField] GameObject[] pieces;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.TryGetComponent(out PlayerController player))
         {
             player.HitObstacle();
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        GetComponent<Collider>().enabled = false;
+        barrier.SetActive(false);
+        foreach (var piece in pieces)
+        {
+            piece.SetActive(true);
+            var rb = piece.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.AddExplosionForce(5f, transform.position, 2f);
         }
     }
 }
