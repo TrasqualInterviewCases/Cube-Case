0ffa0b070da68b097309114d44db26ea3b430a3c
Trasqual <59961702+Trasqual@users.noreply.github.com>
2022-05-21 08:55:03 +0300

    Working on UI Elements

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,15 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Start Panel Fields")]
     [SerializeField] GameObject startPanel;
+    [SerializeField] TMP_Text levelText;
+
+    [Header("Game Panel Fields")]
     [SerializeField] GameObject gamePanel;
+    [SerializeField] TMP_Text goldText;
+
+    private void Start()
+    {
+        SetLevelText();
+    }
+
+    private void SetLevelText()
+    {
+        levelText.SetText($"Level {1}");
+    }
+
+    public void SetGoldText()
+    {
+        goldText.SetText(0.ToString());
+    }
 
     private void OnGameStartCallback()
     {
         startPanel.SetActive(false);
+        gamePanel.SetActive(true);
     }
 
     private void OnEnable()
